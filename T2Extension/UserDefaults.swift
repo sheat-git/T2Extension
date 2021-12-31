081909093255afc5be8ea8032773fa304b57fd8a
ohata <sheat.ohata@gmail.com>
2021-12-31 19:02:13 +0900

    base app

---
+++ T2Extension/UserDefaults.swift
@@ -0,0 +1,16 @@
+//
+//  UserDefaults.swift
+//  T2Extension
+//
+//  Created by 大畑允人 on 2021/12/31.
+//
+
+import Foundation
+
+extension UserDefaults {
+    
+    static var T2E: Self {
+        return Self.init(suiteName: "group.tokyo.sheat.T2Extension")!
+    }
+    
+}
