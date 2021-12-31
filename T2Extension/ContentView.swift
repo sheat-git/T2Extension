a7172ec2cbde563ce16d519754742c126418fbff
ohata <sheat.ohata@gmail.com>
2021-12-31 15:08:33 +0900

    Initial Commit

---
+++ T2Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  T2Extension
+//
+//  Created by 大畑允人 on 2021/12/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ T2Extension/T2ExtensionApp.swift
@@ -0,0 +1,17 @@
+//
+//  T2ExtensionApp.swift
+//  T2Extension
+//
+//  Created by 大畑允人 on 2021/12/31.
+//
+
+import SwiftUI
+
+@main
+struct T2ExtensionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
