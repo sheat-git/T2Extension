fa3c2efeafcca7bf67b1bc2cf41dd10c465582a7
Yoshito Ohata <yoshito.dev@icloud.com>
2022-01-07 13:36:15 +0900

    add appDelegate

---
+++ T2Extension/MenuBuilder.swift
@@ -0,0 +1,13 @@
+//
+//  MenuBuilder.swift
+//  T2Extension
+//
+//  Created by 大畑允人 on 2022/01/07.
+//
+
+import UIKit
+
+extension AppDelegate {
+    override 
+    
+}
--- T2Extension/T2ExtensionApp.swift
+++ T2Extension/T2ExtensionApp.swift
@@ -9,9 +9,26 @@ import SwiftUI
 
 @main
 struct T2ExtensionApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             HomeView()
         }
     }
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    #if targetEnvironment(macCatalyst)
+    override func buildMenu(with builder: UIMenuBuilder) {
+        guard builder.system == UIMenuSystem.main else { return }
+        builder.remove(menu: .file)
+        builder.remove(menu: .edit)
+        builder.remove(menu: .format)
+        builder.remove(menu: .view)
+        builder.remove(menu: .window)
+        builder.remove(menu: .help)
+    }
+    #endif
 }
