0556ed528b2f607df9a8a158fbdd91a8004d6fcd
sheat-git <sheat.ohata@gmail.com>
2022-01-23 00:39:00 +0900

    migration UD -> KC

--- T2Extension/T2ExtensionApp.swift
+++ T2Extension/T2ExtensionApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import KeychainAccess
 
 @main
 struct T2ExtensionApp: App {
@@ -20,6 +21,29 @@ struct T2ExtensionApp: App {
 }
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        let oldSchemaVersion: Int = UserDefaults.T2E.object(forKey: "SchemaVersion") as? Int ?? 1
+        if (oldSchemaVersion < 2) {
+            let appIdentifierPrefix = Bundle.main.infoDictionary!["AppIdentifierPrefix"] as! String
+            let keychain = Keychain(service: "tokyo.sheat.T2Extension", accessGroup: appIdentifierPrefix + "T2AccountShareGroup")
+            for key in ["Account", "Password"] {
+                try? keychain.set(UserDefaults.T2E.string(forKey: key) ?? "", key: key)
+                UserDefaults.T2E.removeObject(forKey: key)
+            }
+            for n in (1...7) {
+                let key = "Row\(n)"
+                try? keychain.set(UserDefaults.T2E.string(forKey: key) ?? "", key: key)
+                UserDefaults.T2E.removeObject(forKey: key)
+            }
+        }
+        if (oldSchemaVersion != 2) {
+            UserDefaults.T2E.set(2, forKey: "SchemaVersion")
+        }
+        return true
+    }
+    
     #if targetEnvironment(macCatalyst)
     override func buildMenu(with builder: UIMenuBuilder) {
         guard builder.system == UIMenuSystem.main else { return }
